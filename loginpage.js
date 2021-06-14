279a5a7cb4cf83e84ec4c54acda2e9dcbda4381c
tsmillwala <69706508+tsmillwala@users.noreply.github.com>
2021-06-14 22:37:23 +0530

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -9,7 +9,7 @@ window.location = "https://www.petmate.in/"; // Redirecting to other page.
 return false;
 }
 else{
-attempt --;//, Decrementing by one.
+attempt --;//,, decrementing by one.
 alert("Invalid credintials! You have "+attempt+" attempts left.");
 
 // Disabling fields after 3 attempts.
