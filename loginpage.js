a17c00a18b30ff66b046cf04d215479e498365b3
tsmillwala <69706508+tsmillwala@users.noreply.github.com>
2021-06-15 06:12:50 +0530

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -4,20 +4,22 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "jamea" && password == "saifiyah" ){
-alert ("Login successfully. You will be redirected once you click OK.");
+sweetAlert("Login successfully. You will be redirected once you click OK.");
+else if( username == "khaimat" && password == "riyadat") {
+sweetAlert("Login successfully. You will be redirected once you click OK.");
 window.location = "https://www.petmate.in/"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;//,, decrementing by one.
-alert("Invalid credintials! You have "+attempt+" attempts left.");
+sweetAlert("Invalid credintials! You have "+attempt+" attempts left.");
 
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("login").disabled = true;
-alert("You entered wrong credintials 3 times. Now, the form is disabled.")
+SweetAlert("You entered wrong credintials 3 times. Now, the form is disabled.")
 return false;
 }
 }
